2dbf3eef1e2c4ef49f66d97ce6abe545130a0c2b
vimalrajmanokaran <47937879+vimalrajmanokaran@users.noreply.github.com>
2020-04-06 10:48:06 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+      int a=in.nextInt();
+      int b=in.nextInt();
+      int c=(a*b)/2;
+      System.out.print(c);
+      
+    }
+}
