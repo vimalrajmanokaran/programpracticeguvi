924c8c237317ef52ca8cb80f03c14cf9cb556a09
vimalrajmanokaran <47937879+vimalrajmanokaran@users.noreply.github.com>
2020-04-06 11:00:46 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
       Scanner in=new Scanner(System.in);
-      int a=in.nextInt();
-      int b=in.nextInt();
+      double a=in.nextInt();
+      double b=in.nextInt();
       if(a>=1 && b>=1)
       {
-      int c=(a*b)/2;
+      double c=(a*b)/2;
       System.out.print(c);
       }
       else
