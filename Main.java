34fbffbb334427ec066c0714b83d8e946119a054
vimalrajmanokaran <47937879+vimalrajmanokaran@users.noreply.github.com>
2020-04-06 10:54:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,14 @@ public class Main {
       Scanner in=new Scanner(System.in);
       int a=in.nextInt();
       int b=in.nextInt();
+      if(a>=1 && b>=1)
+      {
       int c=(a*b)/2;
       System.out.print(c);
-      
+      }
+      else
+      {
+       System.out.print("Check the number");   
+      }
     }
 }
